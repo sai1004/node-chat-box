bd094b7bfbb698135d62fd6168923dba3f68c965
sai1004 <saikiran.bavandla@gmail.com>
2019-08-21 19:16:57 +0530

    commit

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+// var app = require("express")();
+// var http = require("http").createServer(app);
+// var io = require("socket.io")(http);
+// var os = require('os');
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/templates/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/templates/index.html");
+// });
 
-io.on("connection", socket => {
-  console.log("a user connected");
-});
+// io.on("connection", socket => {
+//   console.log("a user connected");
+// });
 
-io.on("connection", function(socket) {
-  socket.on("chat message", function(msg , smsg) {
-    console.log("message: " + msg , smsg );
-  });
-});
+// io.on("connection", function(socket) {
+//   socket.on("chat message", function(msg ) {
+//     console.log("message: " + msg  );
+//   });
+// });
 
-io.emit("some event", { for: "everyone" });
+// io.emit("some event", { for: "everyone" });
 
-io.on("connection", function(socket) {
-  socket.broadcast.emit("hi");
-});
+// io.on("connection", function(socket) {
+//   socket.broadcast.emit("hi");
+// });
 
-io.on("connection", function(socket) {
-  socket.on("chat message", function(msg , smsg) {
-    io.emit("chat message", msg , smsg);
-  });
-});
+// io.on("connection", function(socket) {
+//   socket.on("chat message", function(msg ) {
+//     io.emit("chat message", msg );
+//   });
+// });
 
-http.listen(3000, '0.0.0.0' , () => {
-  console.log(" server listening on port: 3000");
-});
+// http.listen(3000, '0.0.0.0' , os.hostname , () => {
+//   console.log(" server listening on port: 3000");
+// });
+
+
