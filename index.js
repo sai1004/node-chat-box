9e6e46e231142530449735f2719ba1f04920de7c
sai1004 <saikiran.bavandla@gmail.com>
2019-10-09 15:10:04 +0530

    commit

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-// var app = require("express")();
-// var http = require("http").createServer(app);
-// var io = require("socket.io")(http);
-// var os = require('os');
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var os = require('os');
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/templates/index.html");
-// });
+var port = 4040;
 
-// io.on("connection", socket => {
-//   console.log("a user connected");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/templates/index.html");
+});
 
-// io.on("connection", function(socket) {
-//   socket.on("chat message", function(msg ) {
-//     console.log("message: " + msg  );
-//   });
-// });
+io.on("connection", socket => {
+  console.log("a user connected");
+});
 
-// io.emit("some event", { for: "everyone" });
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg ) {
+    console.log("message: " + msg  );
+  });
+});
 
-// io.on("connection", function(socket) {
-//   socket.broadcast.emit("hi");
-// });
+io.emit("some event", { for: "everyone" });
 
-// io.on("connection", function(socket) {
-//   socket.on("chat message", function(msg ) {
-//     io.emit("chat message", msg );
-//   });
-// });
+io.on("connection", function(socket) {
+  socket.broadcast.emit("hi");
+});
 
-// http.listen(3000, '0.0.0.0' , os.hostname , () => {
-//   console.log(" server listening on port: 3000");
-// });
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg ) {
+    io.emit("chat message", msg );
+  });
+});
+
+http.listen(port, '0.0.0.0' , os.hostname , () => {
+  console.log(`server listening on port: ${port}`);
+});
 
 
